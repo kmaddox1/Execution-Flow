44f44ddc30b827db6b3e3f2ff6ebc9b1265693d5
Kimberly Maddox <160989452+kmaddox1@users.noreply.github.com>
2024-04-30 22:47:40 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+alert("I");
+alert("Have");
+alert("Execution");
+alert("Flow");
+
+<!-- Each time you run the code, the browser will execute the code from the top to the bottom. Evertime you click ok it will move to the next line. -->
